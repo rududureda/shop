6e4cc6f1658390bce27ac3763f1aea08e09f1d2c
rududureda <ranc.reda@gmail.com>
2024-02-20 22:55:33 +0200

    adding fetchUser

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.scss';
 
 function Navbar() {
+  const [user, setUser] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users/8'
+        );
+        if (!response.ok) throw new Error('Something went wrong');
+        const data = await response.json();
+
+        console.log(data);
+
+        setUser(data.username);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <nav className="nav-container">
       <h1>My shop</h1>
+
       <ul>
         <li>
           <NavLink to="/">All items</NavLink>
@@ -17,6 +43,7 @@ function Navbar() {
           <NavLink to="/favorites">Favorite</NavLink>
         </li>
       </ul>
+      {loading ? <h1 className="loading">loading...</h1> : <h1>{user}</h1>}
     </nav>
   );
 }
