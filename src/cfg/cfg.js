78e1d98c465ba7f2a61b10ac2fb413f87c19dfc3
rududureda <ranc.reda@gmail.com>
2024-03-25 21:09:21 +0200

    add -space

--- src/cfg/cfg.js
+++ src/cfg/cfg.js
@@ -1,8 +1,8 @@
-export const cfg={
-    API:{
-        HOST:process.env.NODE_ENV==='production'
-        ? 'https://api-shop-fire.vercel.app '
-        : 'http://localhost:3000'
-    },
-
-}
+export const cfg = {
+  API: {
+    HOST:
+      process.env.NODE_ENV === 'production'
+        ? 'https://api-shop-fire.vercel.app'
+        : 'http://localhost:3000',
+  },
+};
